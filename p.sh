61f5c329a59770616800a4c0a71f72c15dcf2ae6
gxvigo <gxvigo@gmail.com>
2017-01-09 17:19:24 +1300

    test change for Jenkins

--- p.sh
+++ p.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 echo "Start publishing wp-banking-services product"
 apic login --username student@think.ibm --password Passw0rd! --server 192.168.225.100
 apic publish banking-services_1.0.0.yaml --catalog sb --organization sales --server 192.168.225.100
